393b293711ab84b820f10b3ce7af12a472335642
Sainath Guddeti <130617791+Sainath944@users.noreply.github.com>
2025-05-08 12:13:27 +0530

    Update try.py

--- leaf_disease/try.py
+++ leaf_disease/try.py
@@ -7,7 +7,7 @@
 
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyAS7UP1sNxVjJCXm59s22zPvByipL1f6Mg")
+genai.configure(api_key="your-api-key")
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Explain how AI works")
 print(response.text)
